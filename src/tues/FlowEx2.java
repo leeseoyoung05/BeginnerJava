6f62d726c3833c64f4d29d1de2d84faefd26166c
Rommmu <tonty1222@naver.com>
2024-03-26 16:21:31 +0900

    반복문 예제

---
+++ src/tues/FlowEx2.java
@@ -0,0 +1,11 @@
+package tues;
+
+public class FlowEx2 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i++ != 10) {
+            System.out.println(i);
+        }
+    }
+}
