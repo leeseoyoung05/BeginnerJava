366cff6b8d0acfb834f33d30a6cf9043cb81af9c
Rommmu <tonty1222@naver.com>
2024-03-27 14:10:04 +0900

    과제 파일

---
+++ src/assignments/Test1.java
@@ -0,0 +1,64 @@
+package assignments;
+
+// 다음 조건을 만족하도록 Cylinder 클래스를 작성합니다.
+class Cylinder {
+
+	// 원주율 3.14를 정적 상수 PI로 선언과 동시에 초기화
+
+    // 정수형 원의 중심 좌표 x, y를 선언
+
+    // 실수형 원의 반지름 r을 선언
+
+    // 정수형 원기둥의 높이 height를 10으로 선언과 동시에 초기화
+
+
+	// 생성자1: 정수 인자 x, y 와 실수 인자 r을 전달 받아서 해당 필드 값을 초기화
+
+
+
+    // 생성자1: 정수 인자 x, y, height 와 실수 인자 r을 전달 받아서 해당 필드 값을 초기화
+
+
+	// circleArea 메서드: PI를 이용하여 원의 면적 반환
+
+
+    // volume 메서드: 면적과 높이를 이용하여 부피를 반환
+
+
+    // surfaceArea 메서드: PI를 이용하여 원기둥의 겉넓이를 반환
+
+
+    // move 메서드: 정수 인자 dx, dy를 전달 받아서 원의 중심 좌표를 이동
+    // - 예: 필드 x가 1이고 dx가 10이면 x는 11로 변경되어야 함
+    // 객체 자신을 반환
+
+	void print() {
+		System.out.println("<"+x+","+y+":"+r+">");
+	}
+}
+
+
+// 메인메서드 수행시 이렇게 출력되도록
+//<13,15:6.0>
+//113.04
+//1130.4
+//602.88
+//<17,25:10.0>
+//314.0
+//1570.0
+//942.0
+public class Test1 {
+	public static void main(String[] args) {
+        Cylinder c1 = new Cylinder(3,5, 6);
+		c1.move(10,10).print();
+		System.out.println(c1.area());
+        System.out.println(c1.volume());
+        System.out.println(c1.surfaceArea());
+
+        Cylinder c2 = new Cylinder(5,8, 10, 5);
+        c2.move(12,17).print();
+        System.out.println(c2.area());
+        System.out.println(c2.volume());
+        System.out.println(c2.surfaceArea());
+	}
+}
